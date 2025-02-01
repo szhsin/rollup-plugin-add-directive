a98c04790e3f6d7cb9145aead198d1d75e2e374c
Zheng Song <41896553+szhsin@users.noreply.github.com>
2025-02-01 15:28:55 +1100

    Add jsdoc comments to types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,18 @@
 import { Plugin } from 'rollup';
 import { Options } from 'micromatch';
 export interface PluginOptions {
+    /**
+     * @default "'use client';"
+     */
     directive?: string;
+    /**
+     * Glob pattern
+     */
     pattern?: string | readonly string[];
+    /**
+     * The options from micromatch
+     * @link https://github.com/micromatch/micromatch?tab=readme-ov-file#options
+     */
     options?: Options;
 }
 declare const addDirective: ({ directive, pattern, options }?: PluginOptions) => Plugin;
